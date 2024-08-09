c96d9ca13443f4ef5c57a7e4062bed69eacc7e83
Deepakraja03 <12306deepakraja@gmail.com>
2024-08-09 11:20:42 +0530

    ethers added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { AptosClient, AptosAccount, HexString } = require('aptos');
 const cors = require('cors');
+const { ethers } = require('ethers');
 
 const app = express();
 const port = 5000;
